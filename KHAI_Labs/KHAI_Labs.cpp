300fb3fbd1cf0a4142dc872aec0df5fa6df25aa6
BohdanLytvynov <blitvinov19@gmail.com>
2025-02-08 19:42:53 +0200

    lab 1 I add the GUI app to my lab also I add bootstrap css and js files to the project.

--- KHAI_Labs/KHAI_Labs.cpp
+++ KHAI_Labs/KHAI_Labs.cpp
@@ -7,38 +7,14 @@
 
 int main()
 {
-    using namespace ds;
-
     int flag = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
     flag |= _CRTDBG_LEAK_CHECK_DF;
     _CrtSetDbgFlag(flag);
-   
-    ds::linear_ds::single_linked_list<int> sll;
 
-    std::exception err;
+    using namespace ds;
+    using namespace std;
 
-    int r = sll.addToStart(1, err);
-
-   if (r == 0)
-   {
-        int len = sll.length();
-        int* res = new int[len];
-
-        if (sll.ToArray(res, err) == 0)
-        {
-            for (size_t i = 0; i < len; i++)
-            {
-                printf("Value is: %s\n", res[i]);
-            }
-        }
-   }
-   else
-   {
-        printf("error on adding %s\n", err.what());
-   }
-
-    
-
+     
 }
 
 
