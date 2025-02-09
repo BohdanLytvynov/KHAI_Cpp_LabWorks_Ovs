8b8af891306f86e9655d70e8e3f21c6e20047e77
BohdanLytvynov <blitvinov19@gmail.com>
2025-02-10 00:25:57 +0200

    lab 1 pushing build prj

--- Lab1/assets/js/app.js
+++ Lab1/assets/js/app.js
@@ -1,60 +1,60 @@
 
-const pathToLocFiles = "../localization/";
-var elements = new Map();
 var loc_files = new Map();
-document.addEventListener("DOMContentLoaded", (event) => {
-    initElements();
-    setLocFiles();
 
-});
-
-function initElements()
-{
-    elements.set("title", getElementById("title"));
-    elements.set("langs", getElementById("langs"));
-    elements.set("description", getElementById("description"));
-    elements.set("description", getElementById("description"));
-    elements.set("lv-title", getElementById("lv-title"));
-    elements.set("add-btn", getElementById("add-btn"));
-    elements.set("edit-btn", getElementById("edit-btn"));
-    elements.set("del-btn", getElementById("del-btn"));
-    elements.set("list-view", getElementById("list-view"));
-
-    //Check elements init
-    console.log("Elements were succesfully inited:");
-    elements.forEach((v, k ,m)=>
-    {
-        console.log(k);
+function createBindings()
+{    
+    //Set Reactive Function for Select Language
+    getElementById("langs").addEventListener("change", (event) =>{
+        let language = elements["langs"].value;
+        setLocalization(language);
     });
 }
 
 function setLocalization(localization)
 {
     let file = loc_files[localization]
-    
+    let mainPage = file["main-page"]
     //Set new title
-    elements["title"].innerHTML = loc_files["title"]
+    getElementById("title").innerHTML = mainPage["title"]    
+    //Set languages
+    let langs = mainPage["langs"]
     
-    
-}
+    let count = langs.length;
 
-function setLocFiles()
-{
-    fetch(pathToLocFiles + "loc_files.json")
-    .then(resp => resp.json())
-    .then(data => {
-        for (const locFile in data) {
-            fetch(pathToLocFiles + locFile)
-            .then(resp => resp.json())
-            .then(jsonData =>
-                {
-                    loc_files.set(locFile["language"], jsonData);
-                })
-            .catch(error => console.log(`Unable to parse: ${pathToLocFiles + locFile}`));
+    for(let i = 0; i < count; i++)
+    {
+        let opt = document.createElement("option");
+        if(i === 0) 
+        {
+            opt.selected = true;            
         }
-    }).catch(error => console.error("Unable to fetch data about localization!"));
 
-    console.log("Localization files parsed.")
+        opt.innerHTML = langs[i];
+        opt.value = langs[i];
+
+        getElementById("langs").appendChild(opt);
+    }
+
+    //Set description
+    getElementById("description").innerHTML = mainPage["description"]
+    //Set List View Title
+    getElementById("lv-title").innerHTML = mainPage["lv-title"]
+    let buttons = mainPage["buttons"]
+    //Set Add Button
+    getElementById("add-btn").innerHTML = buttons["add-btn"]
+    //Set edit button
+    getElementById("edit-btn").innerHTML = buttons["edit-btn"]
+    //Set dlete button
+    getElementById("del-btn").innerHTML = buttons["del-btn"]
+
+}
+//data - an array of localization settings
+function setLocalizationFiles(data)
+{
+    for (let locFile in data)         
+    {
+        loc_files.set(locFile["language"], locFile);            
+    }
 }
 
 function getElementById(elemId)
