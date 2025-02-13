f9e458c61c2130cb87909056029d7646e8acc6f8
BohdanLytvynov <blitvinov19@gmail.com>
2025-02-13 20:30:22 +0200

    lab1 - We have added boost to CMake. For usage we have to use the env var boostRoot

--- Lab1/build.bat
+++ Lab1/build.bat
@@ -3,14 +3,14 @@ echo Starting build...
 echo Removing old files
 rmdir build /s /q
 echo Old files were removed.
-echo Starting CMAKE build
-set BOOST_ROOT=C:\Users\Administrator\Projects\C++\AddLibs\boost_1_87_0
-set BOOST_LIBRARYDIR=%BOOST_ROOT%\stage\lib
-echo BOOST_ROOT path is %BOOST_ROOT%
-echo BOOST_LIBRARYDIR path is %BOOST_LIBRARYDIR%
+echo Starting CMake build...
 cmake -B build 
 echo CMAKE build finished. New files generated. You can view them in /build folder.
-echo building Debug configuration
-cmake --build build --config Debug
+echo building Debug configuration...
+cmake --build build --config Debug --clean-first
 echo building configuration finished.
 pause
+
+
+
+
